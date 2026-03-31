e89ff764c351c624e7c8d6753607be89a2b2e1c0
Soobin Rho <soobinrho@gmail.com>
2026-03-30 20:15:36 -0500

    refactor: modularize metadata and common responses
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

---
+++ responses.py
@@ -0,0 +1,42 @@
+from fastapi import status
+from fastapi.responses import JSONResponse
+
+# =====
+# Books
+# =====
+RESPONSE_INVALID_PRICE = JSONResponse(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    content={
+        "message": "Invalid price. It must be a valid number, and it must have between 0 to 2 decimal places."
+    },
+)
+
+RESPONSE_INVALID_QUANTITY = JSONResponse(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    content={"message": "Invalid quantity. It must be a valid number."},
+)
+
+# =========
+# Customers
+# =========
+RESPONSE_INVALID_EMAIL = JSONResponse(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    content={
+        "message": 'Invalid email. It must match the regular expression "[^@]+@[^@]+\\.[^@]+".'
+    },
+)
+
+RESPONSE_INVALID_STATE = JSONResponse(
+    status_code=status.HTTP_400_BAD_REQUEST,
+    content={
+        "message": "Invalid state. It must be a valid 2-letter U.S. state abbreviation."
+    },
+)
+
+# ===
+# BFF
+# ===
+RESPONSE_UNAUTHORIZED = JSONResponse(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    content={"message": "Please provide a valid JWT."},
+)
